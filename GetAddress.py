ecc4381ceba4fc8eb87f4d6ce9e1bbd2f2711b16
zhhony <zhhony@126.com>
2022-06-14 15:50:28 +0800

    优化函数说明

--- GetAddress.py
+++ GetAddress.py
@@ -9,7 +9,10 @@ conf = Config('D:\\workdata\\pyget_address\\config.json')
 
 
 def GetSuggest(query: str, region: str = '北京市') -> list:
-    """根据提供的地区关键字，以及地区权重，获取推荐的地区名称清单"""
+    """根据提供的地区关键字，以及地区权重，检索某一行政区划内（目前最细到城市级别）的地点信息。\n
+    query:检索关键字。行政区划区域检索不支持多关键字检索。
+    region:检索行政区划区域,增加区域内数据召回权重。
+    """
 
     http = conf.getSuggestion
     output = conf.getOutput
@@ -28,7 +31,10 @@ def GetSuggest(query: str, region: str = '北京市') -> list:
 
 
 def GetLatitude(address: str,  city: str = '北京市') -> any:
-    """根据提供的地址返回对应的经纬度"""
+    """根据提供的结构化地址数据（如：北京市海淀区上地十街十号）转换为对应坐标点（经纬度）。\n
+    address:待解析的地址。最多支持84个字节。
+    city:地址所在的城市名。用于指定上述地址所在的城市，当多个城市都有上述地址时，该参数起到过滤作用，但不限制坐标召回城市。
+    """
 
     http = conf.getGeocode
     output = conf.getOutput
@@ -50,7 +56,8 @@ def GetLatitude(address: str,  city: str = '北京市') -> any:
 
 
 def GetSearch(query: str, tag: str, region: str = '北京市') -> any:
-    """本函数用于获取详细的搜索结果。结果将保存自config文件所配置的log路径中。注意，本函数最多能查询150条记录，但是受限于篇幅，将仅仅转存前20条记录。
+    """本函数可通过接口获取地点（POI）基础或详细地理信息。函数返回查询到的条目数，并将详细结果保存到config文件所配置的log路径中。
+    注意，虽然本函数最多能查询150条记录，但是受限于篇幅，将仅仅转存前20条记录。\n
     query:搜索关键字，如天安门
     tag:分类偏好，如美食
     regin:行政区域划分，如北京市
@@ -83,8 +90,10 @@ def GetSearch(query: str, tag: str, region: str = '北京市') -> any:
 
 
 def GetGeocode(location_lat: float, location_lng: float) -> str:
-    """本函数用于利用提供的经纬度获取相应的地理信息并返回格式化地址信息（省、市、区&县、镇&路）。详细结果将保存在config文件所配置的log路径中。\n
-    全球逆地理编码服务是一类Web API接口服务；逆地理编码服务提供将坐标点（经纬度）转换为对应位置信息（如所在行政区划，周边地标点分布）功能。服务同时支持全球行政区划位置描述及周边地标POI数据召回（包括中国在内的全球200多个国家地区）
+    """本函数用于利用提供的经纬度获取相应的地理信息并返回格式化地址信息（省、市、区&县、镇&路）。详细结果将保存在config文件所配置的log路径中。
+    逆地理编码服务提供将坐标点（经纬度）转换为对应位置信息（如所在行政区划，周边地标点分布）功能。\n
+    location_lat：地理经度，小数类型。
+    location_lng：地理纬度，小数类型。
     """
 
     http = conf.getReverseGeocode
