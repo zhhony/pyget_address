e2aa9a87ffcd6ab5ca6f980f689a15c96bd4eaeb
zhhony <zhhony@126.com>
2022-06-16 11:48:32 +0800

    完善错误处理程序，增加对Get方法访问错误的处理办法

--- GetPath.py
+++ GetPath.py
@@ -1,13 +1,18 @@
 import json
+import traceback
 import requests
 from . modules import *
+import sys
 
 # 载入全局参数
 conf = Config('D:\\workdata\\pyget_address\\config.json')
 
 # 定义一个用于反馈API接口成功接入但是返回异常的错误类
+
+
 class PostError(Exception):
     pass
+
 
 def getCarPath(plate_number: str, tactics: int, origin: tuple, destination: tuple, *waypoints: list):
     '''根据提供的信息获取驾车路线规划,并将详细信息存入config文件所定位的log文件夹中\n
@@ -50,10 +55,19 @@ def getCarPath(plate_number: str, tactics: int, origin: tuple, destination: tupl
         re = s.get(https)
         reJson = re.text.encode('utf8')
         reJsonDict = json.loads(reJson)
-        if reJsonDict['status']!=0:
+        if reJsonDict['status'] != 0:
             raise PostError
+    except PostError:
+        print('API返回了错误的信息\n错误码：%s\n错误信息：%s' %
+              (reJsonDict['status'], reJsonDict['message']))
+        return None
     except:
-        print('API返回了错误的信息\n错误码：%s\n错误信息：%s'%(reJsonDict['status'],reJsonDict['message']))
+        print("GET阶段发生了未知的异常")
+        a,b,c = sys.exc_info()
+        print("异常类：%s"%a)
+        print("异常信息：%s"%b)
+        print("\n传播路径:")
+        traceback.print_tb(c)
         return None
     else:
         with open(conf.getLog + 'getCarPath_' + TimeStamp() + '.json', 'w') as file:
@@ -63,5 +77,6 @@ def getCarPath(plate_number: str, tactics: int, origin: tuple, destination: tupl
     finally:
         pass
 
+# from pyget_address import *
 # importlib.reload(pyget_address)
 # pyget_address.getCarPath('苏B37F34',3,(40.01116,116.339303),(39.936404,116.452562))
